c9849710c87584dcbb3f6ae8d37b173a05dde05a
aberweger <145767058+aberweger@users.noreply.github.com>
2024-05-18 13:18:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-	res.send('Hello FS2024 DevOps Course! :-))) (((-:');
+	res.send('Hello FS2024 DevOps Course! Dies ist das zweite Semester. Bald ist Halbzeit! ');
 });
 
 app.listen(PORT, HOST);
